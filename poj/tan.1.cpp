98d434c94b7b7336fe26205cad2f04ad591468f4
ILOT-code <82327624+ILOT-code@users.noreply.github.com>
2021-11-23 16:38:35 +0800

    poj 1183

---
+++ poj/tan.1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    long long a,A,t;
+    cin>>a;
+    A=a*a+1;
+    t=a;
+    while(A%t)
+        t--;
+    cout<<t+A/t+2*a;
+    return 0;
+}
